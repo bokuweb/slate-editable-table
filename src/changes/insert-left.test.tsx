4a805d53f343e8954d4f9a1df7b69a3d293e0445
bokuweb <bokuweb12@gmail.com>
2019-08-27 12:06:55 +0900

    spec: Add insert left spec

--- src/changes/insert-left.test.tsx
+++ src/changes/insert-left.test.tsx
@@ -1,12 +1,15 @@
-import { Editor, Value, ValueJSON } from 'slate';
+import { Editor, Value, Block } from 'slate';
 import { EditTable, hasTablePlugin } from '../index';
-import value from '../../mocks/two-by-two';
 import { createHtml } from './test-helper';
+import { TableLayout } from '../layout';
+
+import twoByTwo from '../../mocks/two-by-two';
+import threeByThreeMerged from '../../mocks/three-by-three-colspan-rowspan-2';
 
 describe('insert-left', function() {
   const plugin = EditTable();
   it('insert column at the left end of the table', function() {
-    const editor = new Editor({ value: Value.fromJSON(value as ValueJSON), plugins: [plugin] });
+    const editor = new Editor({ value: Value.fromJSON(twoByTwo), plugins: [plugin] });
     const cursorBlock = editor.value.document.getDescendant('0');
     if (!cursorBlock) throw new Error('Failed to find block');
     editor.moveTo(cursorBlock.key);
@@ -17,7 +20,7 @@ describe('insert-left', function() {
   });
 
   it('insert column at the right end of the table', function() {
-    const editor = new Editor({ value: Value.fromJSON(value as ValueJSON), plugins: [plugin] });
+    const editor = new Editor({ value: Value.fromJSON(twoByTwo), plugins: [plugin] });
     const cursorBlock = editor.value.document.getDescendant('1');
     if (!cursorBlock) throw new Error('Failed to find block');
     editor.moveTo(cursorBlock.key);
@@ -25,5 +28,45 @@ describe('insert-left', function() {
     const actual = editor.insertLeft().value;
     expect(actual.toJSON()).toMatchSnapshot();
     expect(createHtml(actual)).toMatchSnapshot();
+  });
+
+  it('insert column at the left end of the 3x3 table', function() {
+    const editor = new Editor({ value: Value.fromJSON(threeByThreeMerged), plugins: [plugin] });
+    const cursorBlock = editor.value.document.getDescendant('0');
+    if (!cursorBlock) throw new Error('Failed to find block');
+    editor.moveTo(cursorBlock.key);
+    if (!hasTablePlugin(editor)) return;
+    const actual = editor.insertLeft().value;
+    expect(actual.toJSON()).toMatchSnapshot();
+    expect(createHtml(actual)).toMatchSnapshot();
+
+    const table = TableLayout.create(editor);
+    if (!table) throw new Error('Failed to split cell');
+    expect(table.currentTable.nodes.size).toBe(3);
+    const expectLength = [2, 1, 4];
+    table.currentTable.nodes.forEach((row, i) => {
+      if (!Block.isBlock(row)) throw new Error('Failed to split cell');
+      expect(row.nodes.size).toBe(expectLength[i]);
+    });
+  });
+
+  it('insert column at the right end of the 3x3 table', function() {
+    const editor = new Editor({ value: Value.fromJSON(threeByThreeMerged), plugins: [plugin] });
+    const cursorBlock = editor.value.document.getDescendant('1');
+    if (!cursorBlock) throw new Error('Failed to find block');
+    editor.moveTo(cursorBlock.key);
+    if (!hasTablePlugin(editor)) return;
+    const actual = editor.insertLeft().value;
+    expect(actual.toJSON()).toMatchSnapshot();
+    expect(createHtml(actual)).toMatchSnapshot();
+
+    const table = TableLayout.create(editor);
+    if (!table) throw new Error('Failed to split cell');
+    expect(table.currentTable.nodes.size).toBe(3);
+    const expectLength = [3, 2, 4];
+    table.currentTable.nodes.forEach((row, i) => {
+      if (!Block.isBlock(row)) throw new Error('Failed to split cell');
+      expect(row.nodes.size).toBe(expectLength[i]);
+    });
   });
 });
